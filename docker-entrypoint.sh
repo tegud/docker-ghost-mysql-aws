f5fb329c68e3c7b402437dae0bec2c2c8699fdcf
Steve Elliott <steve@tegud.net>
2017-08-01 15:03:49 +0100

    order is important

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,10 +14,10 @@ if [ ! -e "$GHOST_CONTENT/apps" ]; then
 
 		if [ ! -e "$GHOST_CONTENT/config.js" ]; then
 			sed -r "
+				s/GHOST_DB_HOST/$GHOST_DB_HOST/g;
 				s/GHOST_DB/$GHOST_DB/g;
 				s/GHOST_USER/$GHOST_USER/g;
 				s/GHOST_PASSWORD/$GHOST_PASSWORD/g;
-				s/GHOST_DB_HOST/$GHOST_DB_HOST/g;
 				s!path.join\(__dirname, (.)/content!path.join(process.env.GHOST_CONTENT, \1!g;
 			" "/config-example.js" > "$GHOST_CONTENT/config.js"
 		fi
